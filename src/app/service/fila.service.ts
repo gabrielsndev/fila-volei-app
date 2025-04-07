a9b1e891b2a64cb1e5f4149827a6b74c56ed5f16
Gabriel de Souza <gabrielsndev@gmail.com>
2025-04-07 19:25:33 -0300

    configurando o fila.service.ts

--- src/app/service/fila.service.ts
+++ src/app/service/fila.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class FilaService {
 
-  constructor() { }
+  private fila: string[] = [];
+
+  private filaSubject = new BehaviorSubject<string[]>(this.fila);
+
+  fila$ = this.filaSubject.asObservable();
+
+  constructor() { 
+    this.carregarFila();
+  }
+
+  adicionarJogador(nome: string){
+    this.fila.push(nome);
+    this.salvarFila();
+  }
+
+  private salvarFila() {
+    localStorage.setItem('fila', JSON.stringify(this.fila));
+    this.filaSubject.next(this.fila); 
+  }
+
+  private carregarFila(){
+    const filaSalva = localStorage.getItem('fila');
+    if (filaSalva){
+      this.fila = JSON.parse(filaSalva);
+      this.filaSubject.next(this.fila); 
+    }
+  }
+
+  getFila() {
+    return this.fila;
+  }  
+
+  removerJogador(nome:string){
+    this.fila = this.fila.filter(jogador => jogador !== nome);
+    this.salvarFila();
+  }
 }
