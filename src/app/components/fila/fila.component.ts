333e5ed2a036955c1c64a0e45a86b67180b47041
Gabriel de Souza <gabrielsndev@gmail.com>
2025-04-09 16:23:53 -0300

    Tratando e adicionando a forma de selecionar os jogadores

--- src/app/components/fila/fila.component.ts
+++ src/app/components/fila/fila.component.ts
@@ -12,19 +12,53 @@ export class FilaComponent {
   constructor(private filaService: FilaService) {}
 
   fila: string[] = [];
-
-  ngOnInit(): void {
-    this.filaService.fila$.subscribe(fila => {
-      this.fila = fila;
-    });
-  }
-
   listaTeste = [
     'Maria',
     'João',
     'Lucas',
     'Wagner',
     'Alice'
-  ]
+  ];
+  jogadoresSelecionados: string[] = [];
+
+
+  ngOnInit(): void {
+    this.filaService.fila$.subscribe(fila => {
+      this.fila = fila;
+    });
+
+    if (this.listaTeste.length > 0) {
+      const primeiro = this.listaTeste[0];
+      if (!this.jogadoresSelecionados.includes(primeiro)) {
+        this.jogadoresSelecionados.unshift(primeiro);
+    }
+  }
+  }
+
+
+
+  selecionarJogador(jogador:string): void{
+    const primeiro = this.listaTeste[0];
+    if (jogador === primeiro) {
+      return;
+    }
+    
+    if (this.jogadoresSelecionados.includes(jogador)) {
+      this.jogadoresSelecionados = this.jogadoresSelecionados.filter(j => j !== jogador);
+    } else {
+      if(this.jogadoresSelecionados.length < this.maximoJogadores){
+        this.jogadoresSelecionados.push(jogador);
+      }
+    }
+  }
+
+
+  isSelecionado(jogador: string): boolean {
+    return this.jogadoresSelecionados.includes(jogador);
+  }
+
+  maximoJogadores:number = 4
+
+
 
 }
