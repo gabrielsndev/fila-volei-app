f21e8649fffb9553a930df7bd61eea279be5714f
gabrielsndev <gabrielsndev@gmail.com>
2025-05-07 19:19:22 -0300

    testee

--- src/app/service/config.service.ts
+++ src/app/service/config.service.ts
@@ -8,8 +8,8 @@ import { BehaviorSubject } from 'rxjs';
 export class ConfigService {
   private qtdJogadores: number = 0;
   private descansoBicampeao: boolean = false;
+
   private qtdJogadoresSubject = new BehaviorSubject<number>(this.qtdJogadores);
-  public lalala:string = "";
   private descansoBicampeaoSubject = new BehaviorSubject<boolean>(this.descansoBicampeao);
 
   qtdJogadores$ = this.qtdJogadoresSubject.asObservable();
