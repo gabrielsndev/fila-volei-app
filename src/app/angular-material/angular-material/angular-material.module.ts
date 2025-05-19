0941a9ed79da3d8ad07aff3287fc2ce6acf41b54
gabrielsndev <gabrielsndev@gmail.com>
2025-05-19 20:05:30 -0300

    primeiro visual

--- src/app/angular-material/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material/angular-material.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    MatSlideToggleModule
+    MatSlideToggleModule,
+    MatDividerModule,
+    MatButtonModule
+
   ],
   exports: [
-  MatSlideToggleModule
+  MatSlideToggleModule,
+  MatDividerModule,
+  MatButtonModule
   ]
 })
 export class AngularMaterialModule { }
