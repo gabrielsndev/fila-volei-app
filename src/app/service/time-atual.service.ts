36abf1262417b0f5a9a38514a97d955c8f9a0faa
gabrielsndev <gabrielsndev@gmail.com>
2025-06-02 21:26:10 -0300

    Apagando uns troços

--- src/app/service/time-atual.service.ts
+++ src/app/service/time-atual.service.ts
@@ -3,12 +3,6 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
 
-// Ele precisa ser OUVIDO pelo config.service, pra impedir a muudança de qtd
-// impossível de resolver
-// pra se ligar nas alterações de quantidade de jogadores
-
-// quanto os jogadores selecionados que estão no fila.service
-
 @Injectable({
   providedIn: 'root'
 })
